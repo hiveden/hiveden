04ecb42d0457ec1b3b2927753e595ce8dea1f9d2
Ermal Guni <ermalguni@gmail.com>
2025-11-17 21:26:50 +0100

    Add import config

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"gopkg.in/yaml.v2"
 	"github.com/hiveden/hiveden/internal/docker"
 
 	"github.com/spf13/cobra"
@@ -18,11 +19,22 @@ func main() {
 
 	rootCmd := &cobra.Command{Use: "go-docker-manager"}
 
-	rootCmd.AddCommand(buildListCommand(dockerManager))
-	rootCmd.AddCommand(buildCreateCommand(dockerManager))
-	rootCmd.AddCommand(buildStartCommand(dockerManager))
-	rootCmd.AddCommand(buildStopCommand(dockerManager))
-	rootCmd.AddCommand(buildRemoveCommand(dockerManager))
+	var configFile string
+	rootCmd.PersistentFlags().StringVar(&configFile, "config", "cmd/cli/config.yaml", "config file (default is cmd/cli/config.yaml)")
+
+	containersCmd := &cobra.Command{
+		Use:   "containers",
+		Short: "Manage containers",
+	}
+
+	containersCmd.AddCommand(buildListCommand(dockerManager))
+	containersCmd.AddCommand(buildCreateCommand(dockerManager))
+	containersCmd.AddCommand(buildStartCommand(dockerManager))
+	containersCmd.AddCommand(buildStopCommand(dockerManager))
+	containersCmd.AddCommand(buildRemoveCommand(dockerManager))
+	containersCmd.AddCommand(buildRunAllCommand(dockerManager, &configFile))
+
+	rootCmd.AddCommand(containersCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -102,6 +114,49 @@ func buildRemoveCommand(dm *docker.DockerManager) *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return dm.RemoveContainer(cmd.Context(), args[0])
+		},
+	}
+}
+
+type ContainerConfig struct {
+	Name  string `yaml:"name"`
+	Image string `yaml:"image"`
+}
+
+type Config struct {
+	Containers []ContainerConfig `yaml:"containers"`
+}
+
+func buildRunAllCommand(dm *docker.DockerManager, configFile *string) *cobra.Command {
+	return &cobra.Command{
+		Use:   "run-all",
+		Short: "Create and start all containers from a config file",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			data, err := os.ReadFile(*configFile)
+			if err != nil {
+				return fmt.Errorf("failed to read config file: %w", err)
+			}
+
+			var config Config
+			if err := yaml.Unmarshal(data, &config); err != nil {
+				return fmt.Errorf("failed to unmarshal config: %w", err)
+			}
+
+			for _, containerConfig := range config.Containers {
+				fmt.Printf("Creating container %s with image %s...\n", containerConfig.Name, containerConfig.Image)
+				resp, err := dm.CreateContainer(cmd.Context(), containerConfig.Image, containerConfig.Name)
+				if err != nil {
+					log.Printf("Failed to create container %s: %v", containerConfig.Name, err)
+					continue
+				}
+
+				fmt.Printf("Starting container %s (%s)...\n", containerConfig.Name, resp.ID[:12])
+				if err := dm.StartContainer(cmd.Context(), resp.ID); err != nil {
+					log.Printf("Failed to start container %s: %v", containerConfig.Name, err)
+				}
+			}
+
+			return nil
 		},
 	}
 }
--- go.mod
+++ go.mod
@@ -58,6 +58,7 @@ require (
 	golang.org/x/text v0.28.0 // indirect
 	golang.org/x/time v0.14.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gotest.tools/v3 v3.5.2 // indirect
 )
